2467f159e3449636e34bcfb270c56f4b300e32c4
wyyl1 <289460202@qq.com>
2024-03-12 11:57:30 +0800

    first commit

--- app/src/main/java/com/aoeai/isn/MainActivity.kt
+++ app/src/main/java/com/aoeai/isn/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello: $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
